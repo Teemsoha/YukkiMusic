5ec79863b34331fa645d0ea2ce396d068d1ab7cd
Teemsoha <163581338+Teemsoha@users.noreply.github.com>
2024-03-27 05:32:25 +0300

    Update Telegram.py

--- YukkiMusic/platforms/Telegram.py
+++ YukkiMusic/platforms/Telegram.py
@@ -1,9 +1,9 @@
 #
-# Copyright (C) 2023-2024 by YukkiOwner@Github, < https://github.com/YukkiOwner >.
+# Copyright (C) 2021-present by TeamYukki@Github, < https://github.com/TeamYukki >.
 #
-# This file is part of < https://github.com/YukkiOwner/YukkiMusicBot > project,
+# This file is part of < https://github.com/TeamYukki/YukkiMusicBot > project,
 # and is released under the "GNU v3.0 License Agreement".
-# Please see < https://github.com/YukkiOwner/YukkiMusicBot/blob/master/LICENSE >
+# Please see < https://github.com/TeamYukki/YukkiMusicBot/blob/master/LICENSE >
 #
 # All rights reserved.
 #
