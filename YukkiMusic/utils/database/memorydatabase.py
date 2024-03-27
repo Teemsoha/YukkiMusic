4d6c60874ae466b6703b0041616e20fd8d32d3e8
Teemsoha <163581338+Teemsoha@users.noreply.github.com>
2024-03-27 03:50:19 +0300

    Update memorydatabase.py

--- YukkiMusic/utils/database/memorydatabase.py
+++ YukkiMusic/utils/database/memorydatabase.py
@@ -460,7 +460,6 @@ async def maintenance_on():
 
 # Audio Video Limit
 from pytgcalls.types import AudioQuality, VideoQuality
-from pytgcalls.types import VideoParameters
 
 
 async def save_audio_bitrate(chat_id: int, bitrate: str):
