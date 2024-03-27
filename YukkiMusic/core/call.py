d2e977d9fb4c0c732245813b8bba401792479127
Teemsoha <163581338+Teemsoha@users.noreply.github.com>
2024-03-27 03:51:41 +0300

    Update call.py

--- YukkiMusic/core/call.py
+++ YukkiMusic/core/call.py
@@ -19,8 +19,7 @@ from pyrogram.errors import (ChatAdminRequired,
 from pyrogram.types import InlineKeyboardMarkup
 from pytgcalls import PyTgCalls
 from pytgcalls.exceptions import (AlreadyJoinedError,
-                                  NoActiveGroupCall,
-                                  TelegramServerError)
+                                  NoActiveGroupCall,)
 from pytgcalls.types import (JoinedGroupCallParticipant,
                              LeftGroupCallParticipant, Update)
 from pytgcalls.types.input_stream import AudioPiped, AudioVideoPiped
